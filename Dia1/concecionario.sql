6c7739e99ba1b1cd3976c210dac6e941e5534a36
jeanFrancoGuerreroAcero <166477787+jeanFrancoGuerreroAcero@users.noreply.github.com>
2024-10-25 13:02:42 -0500

    feat :sparkles: initial work in tables

---
+++ Dia1/concecionario.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE migrupoT2;  -- crear una base de datos
+
+USE migrupoT2; -- utilizar mi base de datos
+
+CREATE TABLE vehiculos(
+VIN INT PRIMARY KEY AUTO_INCREMENT,
+marca VARCHAR(50) NOT NULL,
+modelo VARCHAR(30) NOT NULL,
+Año INT NOT NULL,
+color VARCHAR(20) NOT NULL,
+Tipo_combustible VARCHAR(30) NOT NULL,
+Estado VARCHAR(30) NOT NULL
+);
+
+DESCRIBE vehiculos;
+
+CREATE TABLE mantenimiento(
+ID_MANTENIMIENTO INT PRIMARY KEY AUTO_INCREMENT,
+Tipo_SERVICIO VARCHAR(30) NOT NULL,
+VIN INT NOT NULL,
+FOREIGN KEY (VIN) REFERENCES vehiculos(VIN)
+);
+
+CREATE TABLE vehiculos_clientes(
+ID_VEHICULOS_CLIENTES INT PRIMARY KEY AUTO_INCREMENT,
+ID_vehiculos INT NOT NULL,
+ID_clientes INT NOT NULL,
+FOREIGN KEY (ID_vehiculos) REFERENCES vehiculos(VIN),
+FOREIGN KEY (ID_clientes) REFERENCES vehiculos(VIN)
+);
+
+CREATE TABLE clientes(
+ID_clientes INT PRIMARY KEY AUTO_INCREMENT,
+Nombre VARCHAR(50) NOT NULL,
+telefono INT NOT NULL,
+correo_electronico VARCHAR(100) NOT NULL,
+direccion VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE clientes_VENDEDORES(
+
+
+
